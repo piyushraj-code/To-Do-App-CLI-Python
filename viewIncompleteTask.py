9edb8bd2fe895f7b38c066e9780af8dde4f4c521
Piyush Raj <raj4piyush@gmail.com>
2025-08-07 09:14:35 +0530

    Initial commit

---
+++ viewIncompleteTask.py
@@ -0,0 +1,11 @@
+import json
+from utils import returnToHome
+
+def viewCompleteTask():
+    
+    with open("tasks.json", "r") as file:
+        data = json.load(file)
+        for task in data["tasks"]:
+            if task["status"] == False:
+                print(task)
+    returnToHome()
