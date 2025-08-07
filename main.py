84f89c2a1dc1bb2e9bfca37cf591929912ec3386
Piyush Raj <raj4piyush@gmail.com>
2025-08-07 09:32:26 +0530

    Initial commit

--- main.py
+++ main.py
@@ -13,35 +13,32 @@ def main():
         interface()
         command = int(input("Enter your choice: "))
         if(command == 1):
-            task = input("Enter task to add:")
+            task = input("Enter task to add: ")
             addTask(task)
-            returnToHome()
-
+        
         elif(command == 2):
             task = input("Enter the task to mark as complete: ")
             completeTask(task)
-            returnToHome
 
         elif(command == 3):
             print("Showing all tasks")
             time.sleep(0.2)
             viewAllTask()
-            returnToHome()
         
         elif(command == 4):
             print("Showing incomplete task")
             time.sleep(0.2)
             viewIncompleteTask()
-            returnToHome()
+        
         
         elif(command == 5):
             print("Showing completed task")
             time.sleep(0.2)
             viewCompleteTask()
-            returnToHome()
+        
         
         elif(command == 6):
-            exit
+            break
 
         else:
             print("Enter valid command!")
